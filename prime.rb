1e22cabaa97c210ddcfe13ab660bb82d56b68ff9
sgg2123 <sgg2123@columbia.edu>
2018-04-20 17:22:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@ def prime?(integer)
   divisors = Array(2..(integer.abs-1))
   for num in divisors
     if integer.abs%num == 0
-      return false
-    else 
       return true
+    else 
+      return fals
     end
   end
 end
