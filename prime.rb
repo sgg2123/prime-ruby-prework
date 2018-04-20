8a71167e4e58283eb627d3eb6058243d8cb3c86f
sgg2123 <sgg2123@columbia.edu>
2018-04-20 17:30:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  end
+  divisors = Array(2..(integer-1))
+  for num in divisors
+    if integer%num == 0
+      return false
+    end
+  end
+  return true
+end
